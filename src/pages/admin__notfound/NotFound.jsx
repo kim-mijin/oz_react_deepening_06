9542d93898ccfd2a174bdcd08bfb3cc4632864f4
kim-mijin <mycrimosn@naver.com>
2025-07-05 17:15:17 +0900

    어드민 페이지 연결

--- src/pages/admin__notfound/NotFound.jsx
+++ src/pages/admin__notfound/NotFound.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function NotFound() {
-  return (
-    <div className="not-found">
-      <h1>404 - 잘못된 페이지 요청</h1>
-      <p>이 페이지 요청은 잘못된 페이지 입니다.</p>
-      <Link to="/admin">대시보드로 돌아가기</Link>
+
+const NotFound = () => {
+    return (
+  <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
+      <p>죄송합니다. 해당 페이지는 존재하지 않거나 이동되었습니다.</p>
+      <Link to="/">홈으로 돌아가기</Link>
     </div>
   );
-}
+};
 
 export default NotFound;
